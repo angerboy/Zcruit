c356cf29af4fdf96b766e8e9903b077b6eeaef7e
angerboy <matthewehinger2016@u.northwestern.edu>
2015-12-09 15:11:11 -0600

    html and js changes

--- js/zcruit.js
+++ js/zcruit.js
@@ -1,3 +1,25 @@
+$(window).load(function() {
+	// do any setup when file is loaded
+});
+
+
+/* Add player on click function */
 function calcProbs() {
-	alert("CALCULATE PROBS");
+
+		// instantiate variables
+		var sesStatus;
+
+		// retrieve groups of radio buttons 
+		var sesRadios = document.getElementsByName("ses");
+
+		// find checked values 
+		for(var i = 0, length = sesRadios.length; i < length; i++) {
+			if(sesRadios[i].checked) {
+				sesStatus = parseInt(sesRadios[i].value);
+				break;
+			}
+		}
+
+		console.log(sesStatus);
+
 }
